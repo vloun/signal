e8033892781c7a196ff909de378510e2aeaa26fe
Kevin Bray <kevin.m.bray@gmail.com>
2018-09-11 14:19:56 -0600

    Added REST API deployments to the stack. Updated stub function and Lambda integration.
    https://github.com/vloun/signal/issues/2

--- src/getMenu.js
+++ src/getMenu.js
@@ -1,1 +1,18 @@
-module.exports = function() {};
+exports.handler = ( event, context, callback ) => {
+  console.info('event', event);
+  console.info('context', context);
+
+  callback(null, {
+    statusCode: 201,
+    body: JSON.stringify({
+      foods: [
+        { name: 'carrot' },
+        { name: 'beef' },
+        { name: 'apple' }
+      ]
+    }),
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    }
+  });
+};
